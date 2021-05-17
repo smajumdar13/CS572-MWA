c1090e750535b282b11849e9b734f3b81dcb3e8a
smajumdar13 <77767611+smajumdar13@users.noreply.github.com>
2021-05-17 18:53:58 -0500

    last commit

--- Day1/APP08/app08.js
+++ Day1/APP08/app08.js
@@ -1,5 +1,7 @@
 const child_process = require("child_process");
 
 console.log("1. Start");
-const newProcess = child_process.spawn("node", ["computation/_fibonacci.js"], {stdio: "inherit"});
+const newProcess = child_process.spawn("node", ["computation/_fibonacci.js"], {
+  stdio: "inherit",
+});
 console.log("2. End");
