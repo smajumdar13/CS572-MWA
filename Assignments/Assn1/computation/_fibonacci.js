5ffa58180fdd9c852488d8b91954ce25c8b561f8
smajumdar13 <77767611+smajumdar13@users.noreply.github.com>
2021-05-23 17:42:17 -0500

    Full till day 5

---
+++ Assignments/Assn1/computation/_fibonacci.js
@@ -0,0 +1,9 @@
+const fib = function (number) {
+  if (number <= 2) {
+    return 1;
+  } else {
+    return fib(number - 1) + fib(number - 2);
+  }
+};
+
+console.log("Fibonacci of 42 is " + fib(42));
